809ddc1fb98f9699ff2328ad2a9785a6c644f0e6
Lisandra Melo <mendie73@gmail.com>
2022-12-07 15:25:27 -0300

    Uso de loop em cláusula de switch case para execução do menu

--- src/main.go
+++ src/main.go
@@ -3,9 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	var choice int
-	menu()
-	fmt.Scan(&choice)
+	var choice int = -1
+	for choice != 0 {
+		menu()
+		fmt.Scan(&choice)
+		switch choice {
+		case 1:
+			fmt.Println("1")
+
+		case 2:
+			fmt.Println("2")
+
+		case 3:
+			fmt.Println("3")
+
+		case 4:
+			fmt.Println("4")
+
+		case 5:
+			fmt.Println("5")
+
+		case 6:
+			fmt.Println("6")
+
+		case 7:
+			fmt.Println("7")
+
+		case 8:
+			fmt.Println("8")
+
+		case 0:
+			fmt.Println("0")
+
+		default:
+			fmt.Println("Opção inválida")
+		}
+	}
 }
 
 func menu() {
