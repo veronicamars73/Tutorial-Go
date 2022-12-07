eb88a975b40fbb57e9812f309d1cf505188caa6e
Lisandra Melo <mendie73@gmail.com>
2022-12-07 15:04:47 -0300

    Initial commit - Menu funcional

---
+++ src/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	menu()
+}
+
+func menu() {
+	fmt.Println("Calculadora de Geometria Plana e Espacial")
+	fmt.Println("(1) Triângulo equilátero")
+	fmt.Println("(2) Retângulo")
+	fmt.Println("(3) Quadrado")
+	fmt.Println("(4) Cîrculo")
+	fmt.Println("(5) Pirâmide com base quadrangular")
+	fmt.Println("(6) Cubo")
+	fmt.Println("(7) Paralelepîpedo")
+	fmt.Println("(8) Esfera")
+	fmt.Println("(0) Sair")
+	fmt.Println("")
+	fmt.Println("Digite a sua opção:")
+}
